82647f54b3659d521336db74455ccfa18100d21e
devjhon97 <jhonatanvideos337@gmail.com>
2024-05-03 16:04:20 -0300

    size validation added

--- src/main/java/com/devjhon97/URLShortener/controller/dto/RequestDTO.java
+++ src/main/java/com/devjhon97/URLShortener/controller/dto/RequestDTO.java
@@ -1,4 +1,8 @@
 package com.devjhon97.URLShortener.controller.dto;
 
-public record RequestDTO(String url) {
+import jakarta.validation.constraints.Size;
+
+public record RequestDTO(
+        @Size(min = 8, max = 1000, message = "URL must have between 15 and 1000 characters!")
+        String url) {
 }
